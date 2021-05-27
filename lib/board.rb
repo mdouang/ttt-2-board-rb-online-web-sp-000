228a13e4d11aafabdb729ea095de1e6a9925bf53
Maikaru <maikaru@maikaru.com>
2021-05-27 21:50:59 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,6 +1,11 @@
 # Define the variable board below.
 board="board"
+<<<<<<< HEAD
 board =[" "," "," "," "," "," "," "," "," "]
+=======
+array.new=
+board =["","","","","","","","",""]
+>>>>>>> e57daa47d98ae574035533644132989c74325312
 
 row = ["1", "2" , "3"]
 
